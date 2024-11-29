68751d4eb1e3bdab0702404bdae48af31ede4dab
Misha <mikekapikan@yandex.ru>
2024-11-29 21:20:25 +0300

    prodolzhaem

--- main.py
+++ main.py
@@ -3,14 +3,20 @@ from bs4 import BeautifulSoup
 
 
 def main():
-    url = 'https://tl.rulate.ru/book/16292/327323/ready_new'
+    url = 'https://snusme.eu/'
     response = requests.get(url)
     print(response.status_code)
+    #print(response.text)
 
     soup = BeautifulSoup(response.text, 'html.parser')
-    part = soup.find("div", class_="text-container",)
-    with open("page.txt", 'a', encoding='UTF-8') as file:
-        file.write(part.text)  
+    spoon = soup.find_all("div", class_='flex-col')
+    print([i.text for i in spoon])
+
+    with open("page.txt", "a", encoding='UTF=8') as file:
+        file.write(''.join([i.text for i in spoon]))
+    #part = soup.find("div", class_="text-container",)
+    #with open("page.txt", 'a', encoding='UTF-8') as file:
+        #file.write(part.text)  
 
 
 if __name__ == "__main__":
